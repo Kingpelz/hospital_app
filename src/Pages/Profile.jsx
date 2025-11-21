d121b9cc329c8be785ea49d6073c7be7f318079e
Odufuwa Oluwapelumi <odufuwapelumi@gmail.com>
2025-11-21 03:20:42 +0100

    edited the profile area

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,11 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { data, Link } from "react-router-dom";
 
 function Profile() {
- useEffect (() => {
-  getAllProfile();
-
- }, [])
+ 
 
 
 
